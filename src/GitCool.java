0e26ee1d78f444b3871bd29fce05e0968f52b136
afabri10 <alvarezfabricio1@gmail.com>
2021-08-03 21:32:05 -0400

    added GitHub account

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,4 +3,6 @@ public class GitCool {
     // B23 is a special number
     // B23 is making some sense now
 
+    // We have added our GitHub account under intelliJ
+    // make sure to click apply and click on to save
 }
